58fc5db4e9dfed7a6814fc698375774718f1b629
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-05-29 18:02:05 +0500

    OrderDetail Navbar Created

--- Client/MK/src/Components/NavPaths/OrderDetails.jsx
+++ Client/MK/src/Components/NavPaths/OrderDetails.jsx
@@ -1,13 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const OrderDetails = () => {
+  const [selectedOption, setSelectedOption] = useState("pending");
+
   return (
     <>
-    <div>OrderDetails</div>
-    <i class="fa-solid fa-clock"></i>
-    <i class="fas fa-check-circle"></i>
+    <div className='bg-gradient-to-b p-2 from-[#f8b72c] to-black '>
+    <p className='text-3xl font-bold text-center mb-4'>Order Details</p>
+      <div className="flex justify-center ">
+        <div className="flex flex-wrap justify-center">
+          <button
+            className={`py-2 px-4 rounded-l-lg ${
+              selectedOption === "pending"
+                ? "bg-yellow-500 text-white"
+                : "bg-gray-300"
+            }`}
+            onClick={() => setSelectedOption("pending")}
+          >
+            Pending
+          </button>
+          <button
+            className={`py-2 px-4 ${
+              selectedOption === "approved"
+                ? "bg-green-500 text-white"
+                : "bg-gray-300"
+            }`}
+            onClick={() => setSelectedOption("approved")}
+          >
+            Approved
+          </button>
+          <button
+            className={`py-2 px-4 rounded-r-lg md:rounded-none ${
+              selectedOption === "completed"
+                ? "bg-green-800 text-white"
+                : "bg-gray-300"
+            }`}
+            onClick={() => setSelectedOption("completed")}
+          >
+            Completed
+          </button>
+          <button
+            className={`py-2 px-4 mt-2 md:mt-0 rounded-l-lg md:rounded-none ${
+              selectedOption === "cancelled"
+                ? "bg-red-500 text-white"
+                : "bg-gray-300"
+            }`}
+            onClick={() => setSelectedOption("cancelled")}
+          >
+            Cancelled
+          </button>
+          <button
+            className={`py-2 px-4 mt-2 md:mt-0  rounded-r-lg ${
+              selectedOption === "rejected"
+                ? "bg-red-800 text-white"
+                : "bg-gray-300"
+            }`}
+            onClick={() => setSelectedOption("rejected")}
+          >
+            Rejected
+          </button>
+        </div>
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default OrderDetails
+export default OrderDetails;
