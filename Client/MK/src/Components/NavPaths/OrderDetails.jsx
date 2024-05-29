098c5d39938f189b0cec99cc34abf9f4d8e03084
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-05-29 21:45:00 +0500

    OrderDetail Card Created

--- Client/MK/src/Components/NavPaths/OrderDetails.jsx
+++ Client/MK/src/Components/NavPaths/OrderDetails.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 const OrderDetails = () => {
   const [selectedOption, setSelectedOption] = useState("pending");
-
   return (
     <>
     <div className='bg-gradient-to-b p-2 from-[#f8b72c] to-black '>
@@ -61,9 +60,23 @@ const OrderDetails = () => {
           </button>
         </div>
       </div>
-      </div>
+   
+   <div className='flex flex-wrap  justify-center text-white m-2'>
+      <div className='flex flex-col md:flex-row h-30 my-2 border-gray-100 bg-white rounded-lg' >
+         <div className="rounded-lg m-2 border-2  flex justify-center  shadow-lg">
+            <img className=" md:h-52 w-64 m-3 " src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="product image" />
+        </div>
+        <div className="mt-4 px-5 flex flex-col">
+            <h5 className="text-xl md:text-2xl font-bold mt-2 tracking-tight text-slate-900">Nike Air MX Super 2500 - Red</h5>
+            <h5 className="text-xl mt-1 font-bold text-slate-900">Quatity:</h5>
+            <h5 className="text-xl  mt-1 font-bold text-slate-900">Total:</h5>
+            <h5 className="text-xl  mt-1 font-bold text-slate-900">Order Date:</h5>
+            <button className='bg-red-500 hover:bg-red-800 self-end rounded-md p-2 w-1/2 mt-2 md:mt-12 mb-2'>Cancel Order</button> 
+        </div>
+        </div>        
+     </div>
+    </div>   
     </>
   );
 }
-
 export default OrderDetails;
