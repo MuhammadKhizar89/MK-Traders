aee5d563b3333883075c15637914ef6df97c591d
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-06-07 20:10:29 +0500

    Signup Issue fixed

--- Client/MK/src/Components/Accountomponents/SignUp.jsx
+++ Client/MK/src/Components/Accountomponents/SignUp.jsx
@@ -12,15 +12,18 @@ const SignUp = () => {
   const [address, setAddress] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
+  const [addToCartLoading, setAddToCartLoading] = useState(false);
 
   const handleSignUp = async (event) => {
     event.preventDefault();
     if (password !== confirmPassword) {
-      alert("Passwords do not match");
+      setErrors({ confirmPassword: "Passwords do not match" });
       return;
     }
     try {
+      setAddToCartLoading(true);
       const response = await signup({
         Username: username,
         PhoneNumber: phoneNumber,
@@ -29,19 +32,32 @@ const SignUp = () => {
         Password: password
       });
       if (response.message === 'User registered successfully') {
-        setCookie('token', response.token, { path: '/' });
-        setCookie('email', email, { path: '/' });
-        navigate('/'); // Navigate to the desired page after signup
+        setCookie('token', data.token);
+        setCookie('email', data.Email );
+        setCookie('username', data.Username);
+        navigate('/'); 
       } else {
+        setErrors({ email: response.message });
+        if(response.message=="User with this phone number already exists")
+        setErrors({ phoneNumber: response.message });
+
         console.error('Signup failed:', response.message);
       }
     } catch (error) {
       console.error('Error signing up:', error);
+    }finally{
+      setAddToCartLoading(false);
+
     }
   };
 
   return (
     <>
+     {(addToCartLoading) && (
+  <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="loader"></div>
+  </div>
+)}
       <div className='md:bg-[#f8b72c] px-5 pb-5'>
         <button className='bg-green-500 text-white font-semibold p-3 rounded-md m-5'>
           <Link to='/'>Back</Link>
@@ -57,73 +73,79 @@ const SignUp = () => {
                       <label htmlFor="full-name" className="mb-2 text-lg">Full Name</label>
                       <input
                         id="full-name"
-                        className="border p-3 dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
+                        className={`border p-3 dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full ${errors.username && 'border-red-500'}`}
                         type="text"
                         placeholder="Full Name"
                         required
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                       />
+                      {errors.username && <p className="text-red-500">{errors.username}</p>}
                     </div>
                     <div>
                       <label htmlFor="email" className="mb-2 text-lg">Email</label>
                       <input
                         id="email"
-                        className="border p-3 dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
+                        className={`border p-3 dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full ${errors.email && 'border-red-500'}`}
                         type="email"
                         placeholder="Email"
                         required
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                       />
+                      {errors.email && <p className="text-red-500">{errors.email}</p>}
                     </div>
                     <div>
                       <label htmlFor="address" className="mb-2 text-lg">Address</label>
                       <input
                         id="address"
-                        className="border p-3 dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
+                        className={`border p-3 dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full ${errors.address && 'border-red-500'}`}
                         type="text"
                         placeholder="Home / Shop Address"
                         required
                         value={address}
                         onChange={(e) => setAddress(e.target.value)}
                       />
+                      {errors.address && <p className="text-red-500">{errors.address}</p>}
                     </div>
                     <div>
                       <label htmlFor="contact-no" className="mb-2 text-lg">Contact Number</label>
                       <input
                         id="contact-no"
-                        className="border p-3 dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
-                        type="tel"
+                        className={`border p-3 dark:border-gray-700 shadow-md placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full ${errors.phoneNumber && 'border-red-500'}`}
+                        type="number"
                         placeholder="Contact Number"
                         required
                         value={phoneNumber}
                         onChange={(e) => setPhoneNumber(e.target.value)}
                       />
+                      {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber}</p>}
                     </div>
                     <div>
                       <label htmlFor="password" className="mb-2 text-lg">Password</label>
                       <input
                         id="password"
-                        className="border p-3 shadow-md dark:border-gray-700 placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
+                        className={`border p-3 shadow-md dark:border-gray-700 placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full ${errors.password && 'border-red-500'}`}
                         type="password"
                         placeholder="Password"
                         required
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                       />
+                      {errors.password && <p className="text-red-500">{errors.password}</p>}
                     </div>
                     <div>
                       <label htmlFor="confirm-password" className="mb-2 text-lg">Confirm Password</label>
                       <input
                         id="confirm-password"
-                        className="border p-3 shadow-md dark:border-gray-700 placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full"
+                        className={`border p-3 shadow-md dark:border-gray-700 placeholder:text-base focus:scale-105 ease-in-out duration-300 border-gray-300 rounded-lg w-full ${errors.confirmPassword && 'border-red-500'}`}
                         type="password"
                         placeholder="Confirm Password"
                         required
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
                       />
+                      {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword}</p>}
                     </div>
                     <button
                       className="bg-[#f8b72c] shadow-lg mt-6 hover:border-2 hover:border-black p-2 text-white rounded-lg w-full hover:scale-105 hover:from-purple-500 hover:to-blue-500 transition duration-300 ease-in-out"
