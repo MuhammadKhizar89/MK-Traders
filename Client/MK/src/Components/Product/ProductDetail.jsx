1c59e8aeb9f479d5c6ceda6a7a52989d90fb657a
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2024-06-10 04:11:45 +0500

    Everything just Perfect 2.0

--- Client/MK/src/Components/Product/ProductDetail.jsx
+++ Client/MK/src/Components/Product/ProductDetail.jsx
@@ -8,8 +8,32 @@ import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 import Alert from '../Layout/Alert';
 import '../../App.css';
+import img1 from '../../assets/MKpics/phenyl.png';
+import img2 from '../../assets/MKpics/acid.jpg';
+import img3 from '../../assets/MKpics/hardbroom.jpg';
+import img4 from '../../assets/MKpics/softbroom.jpg';
+import img5 from '../../assets/MKpics/Towel.jpg';
+import img6 from '../../assets/MKpics/Surf Bonus.jpg';
+import img7 from '../../assets/MKpics/TrashBag.jpg';
+import img8 from '../../assets/MKpics/DustCarrier.jpg';
+import img9 from '../../assets/MKpics/washroobrush.jpg';
+import img10 from '../../assets/MKpics/LongDustCarrier.jpg';
+import img11 from '../../assets/MKpics/Wipers.jpg';
+import img12 from '../../assets/MKpics/airFreshners.jpg';
+import img13 from '../../assets/MKpics/Insect Killer(Cobra large)500ml.jpg';
+import img14 from '../../assets/MKpics/MosquitoKiller(Spray)500ml.jpg';
+import img15 from '../../assets/MKpics/handwashliquid.jpg';
+import img16 from '../../assets/MKpics/ScotchBrite.jpg';
+import img17 from '../../assets/MKpics/handtowels.jpg';
+import img18 from '../../assets/MKpics/GlassCleaner.jpg';
+import img19 from '../../assets/MKpics/dusters.jpg';
+import img20 from '../../assets/MKpics/Harpic.jpg';
+import img21 from '../../assets/MKpics/MosquitoKiller(Spray)500ml.jpg';
+
 
 const ProductDetail = () => {
+  const images = [img1, img2, img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15,img16,img17,img18,img19,img20,img21];
+
   const navigate = useNavigate();
   const { productid } = useParams();
   const { getSpecificProduct, buyNow, addToCart,fetchUserInfo } = useApi();
@@ -147,7 +171,7 @@ try{
                 </div>
               </div>
               <div className="mx-auto mb-8 md:mb-1 items-center px-5 md:flex md:p-8">
-                <img className="rounded-md shadow-lg" src={product.Image} alt="Shop image" />
+                <img className="rounded-md shadow-lg" src={images[product.Image]} alt="Shop image" />
               </div>
             </div>
           )}
